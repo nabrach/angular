c08ecdb001afd4aaa2f8f5990b3ede743a6607f2
Nathan Bracher <nrbseal@gmail.com>
2016-08-01 22:26:15 -0500

    Basic Angular Todo

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module('Todo', [])
+.controller('todoController', ['$scope', function($scope){
+  $scope.todos = [
+    {
+      'title': 'Build a todo app',
+      'done': false
+    }
+  ];
+
+  $scope.addTodo = function(){
+    $scope.todos.push({
+      'title': $scope.newTodo,
+      'done': false
+    })
+    $scope.newTodo = ''
+
+  }
+  $scope.clearCompleted = function(){
+    $scope.todos = $scope.todos.filter(function(item){
+      return !item.done
+    })
+  }
+}])
